e83564e61cd41dae6d86cc7065a001462bc21b5a
toidicakhia <toidicakhia1234@gmail.com>
2024-07-30 20:19:42 +0700

    update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,45 +1,46 @@
 <script>
   import "../app.pcss";
   import { Heading, Hr, Img } from "flowbite-svelte";
-  import { HeroHeader, FeatureDefault, FeatureItem } from "flowbite-svelte-blocks";
-  import { Section, Cta } from 'flowbite-svelte-blocks';
-  import { ArrowRightOutline } from 'flowbite-svelte-icons';
+  import { HeroHeader } from "flowbite-svelte-blocks";
+  import { DiscordSolid, GithubSolid } from 'flowbite-svelte-icons';
   import { base } from '$app/paths';
 </script>
 
 <main class="animate-fade-up">
-  <HeroHeader class="hero-header-container mx-auto my-8">
-    <Heading tag="h1" class="hero-heading">
-      A developer team from Vietnam - <span class="highlighted-text">MinusMC</span>
+  <HeroHeader class="bg-white flex flex-col min-h-96 max-w-2xl p-8 rounded-lg shadow-md mx-auto my-8">
+    <div id="about"></div>
+    <Heading tag="h1" class="text-5xl text-center font-bold bg-clip-text text-transparent bg-gradient-to-r from-blue-400 to-blue-700 my-8">
+      A developer team from Vietnam - MinusMC
     </Heading>
-    <p class="hero-description">
+    <p class="text-xl text-gray-600 text-center mb-12">
       MinusMC is a developer team from Vietnam that develops some products such as MinusBounce, LiddellCord, and MinusMC Anticheat Test.
     </p>
   </HeroHeader>
 
-  <Hr classHr="divider"/>
+  <Hr classHr="h-px bg-gray-300 my-8"/>
 
-  <HeroHeader class="products-section px-8 md:px-12 mb-20">
-    <Heading tag="h2" class="section-heading">
+  <HeroHeader class="flex flex-col p-8 md:px-12 mb-20">
+    <div id="products"></div>
+    <Heading tag="h2" class="text-center font-bold text-gray-800 mb-16">
       Our products
     </Heading>
 
-    <div class="product-grid">
-      <div class="product-card">
-        <Img src="{base}/minusbounce.png" class="product-image" alt="MinusBounce" />
-        <div class="product-info">
-          <Heading tag="h3" class="product-title">MinusBounce</Heading>
-          <p class="product-description">
+    <div class="grid grid-cols-1 lg:grid-cols-2 gap-6 md:px-8">
+      <div class="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow-transform-mix hover:-translate-y-1 flex items-center p-4">
+        <Img src="{base}/minusbounce.png" class="h-32 object-cover rounded-lg transition-transform transform hover:scale-105" alt="MinusBounce" />
+        <div class="ml-6">
+          <Heading tag="h3" class="font-bold text-gray-800 mb-2">MinusBounce</Heading>
+          <p class="text-gray-700">
             A free and open source Forge hacked client for Minecraft 1.8.9, bypass many anticheats such as Grim, Matrix, Intave and Vulcan.
           </p>
         </div>
       </div>
 
-      <div class="product-card">
-        <Img src="{base}/liddellcord.png" class="product-image" alt="Liddell Việt" />
-        <div class="product-info">
-          <Heading tag="h3" class="product-title">Liddell Việt</Heading>
-          <p class="product-description">
+      <div class="bg-white rounded-lg shadow-md hover:shadow-lg transition-shadow-transform-mix hover:-translate-y-1 flex items-center p-4">
+        <Img src="{base}/liddellcord.png" class="h-32 object-cover rounded-lg transition-transform transform hover:scale-105" alt="Liddell Việt" />
+        <div class="ml-6">
+          <Heading tag="h3" class="font-bold text-gray-800 mb-2">Liddell Việt</Heading>
+          <p class="text-gray-700">
             A store that sells unbanned Minecraft accounts for Vietnamese cheat community.
           </p>
         </div>
@@ -47,8 +48,20 @@
     </div>
   </HeroHeader>
 
-  <!-- Footer -->
-  <footer class="footer">
-    <p>© minusmc.xyz, idle.zzz ft. toidicakhia All rights reserved. Designed by MinusMC</p>
-  </footer>
+  <HeroHeader class="flex flex-col p-8 md:px-12 mb-20">
+    <div id="contact"></div>
+    <Heading tag="h2" class="text-center font-bold text-gray-800 mb-16">
+      Contact
+    </Heading>
+
+    <div class="grid grid-cols-2 gap-8 text-gray-500 sm:gap-12 dark:text-gray-400">
+      <a href="https://discord.minusbounce.lol" class="flex justify-center items-center">
+        <DiscordSolid class="w-24 h-24"/>
+      </a>
+      <a href="https://github.com/MinusMC" class="flex justify-center items-center">
+        <GithubSolid class="w-24 h-24"/>
+      </a>
+    </div>
+  </HeroHeader>
+
 </main>
--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -9,17 +9,16 @@
   <Navbar color="none" fluid>
     <NavBrand href="{base}/">
       <div class="flex flex-col">
-        <span class="py-0 text-2xl font-medium text-gray-500 hover:text-gray-800 dark:text-gray-400 dark:hover:text-white w-20">MinusMC</span>
+        <span class="py-0 text-2xl font-medium text-gray-500 hover:text-gray-800 transition duration-300 ease-out hover:ease-in">MinusMC</span>
       </div>
     </NavBrand>
 
     <NavHamburger class="text-gray-500 md:block lg:hidden"/>
 
     <NavUl divClass="lg:block w-full lg:w-auto order-2 lg:order-none" ulClass="flex flex-col py-3 my-4 lg:flex-row lg:my-0 text-lg font-medium gap-6 md:px-4">
-      <NavLi class="text-gray-400 hover:text-gray-900 dark:text-gray-400" href="{base}/">Home</NavLi>
-      <NavLi class="text-gray-400 hover:text-gray-900 dark:text-gray-400" href="{base}/">About</NavLi>
-      <NavLi class="text-gray-400 hover:text-gray-900 dark:text-gray-400" href="{base}/">Product</NavLi>
-      <NavLi class="text-gray-400 hover:text-gray-900 dark:text-gray-400" href="{base}/">Contact</NavLi>
+      <NavLi class="text-gray-400 hover:text-gray-900 transition duration-300 ease-out hover:ease-in" href="{base}/#about">About</NavLi>
+      <NavLi class="text-gray-400 hover:text-gray-900 transition duration-300 ease-out hover:ease-in" href="{base}/#products">Products</NavLi>
+      <NavLi class="text-gray-400 hover:text-gray-900 transition duration-300 ease-out hover:ease-in" href="{base}/#contact">Contact</NavLi>
     </NavUl>
   </Navbar>
 </header>
